101272ebbda69b0c958ac0cc6a560e105327bc7c
jodaekyu <jodacu001@gmail.com>
2025-05-28 23:39:22 +0900

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const configuration = new Configuration({
-  apiKey process.env.OPENAI_API_KEY,
+apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
